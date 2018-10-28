6d685b40c77472883d8e1f80dccdfdee612441f8
itsedeug <italogas25@hotmail.com>
2018-10-28 00:19:50 -0300

    Testes de unidade: suíte de testes, teste de inicialização do app, teste de verificação de conteúdo pag inicial. Impl do home controller. Outras modificações.

---
+++ src/main/java/com/itsedeug/swr/SwrSocialNetworkApplication.java
@@ -0,0 +1,12 @@
+package com.itsedeug.swr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwrSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwrSocialNetworkApplication.class, args);
+	}
+}
---
+++ src/test/java/com/itsedeug/swr/SwrSocialNetworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itsedeug.swr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwrSocialNetworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
